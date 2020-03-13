bbee467ccc2296105d8f40af6316943275db5b2a
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-03-13 19:12:24 +0530

    fix the SECOND value

--- leaderboard-aggregator/src/main/java/com/redhat/developers/sql/GameQueries.java
+++ leaderboard-aggregator/src/main/java/com/redhat/developers/sql/GameQueries.java
@@ -251,8 +251,8 @@ public class GameQueries {
             calendar.get(Calendar.MONTH),
             calendar.get(Calendar.DAY_OF_MONTH),
             calendar.get(Calendar.HOUR_OF_DAY),
-            calendar.get(Calendar.SECOND),
-            calendar.get(Calendar.MILLISECOND)),
+            calendar.get(Calendar.MINUTE),
+            calendar.get(Calendar.SECOND)),
         ZoneOffset.ofHoursMinutes(0, 0)));
   }
 }
