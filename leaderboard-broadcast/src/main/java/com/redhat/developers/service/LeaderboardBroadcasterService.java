4beb6e4a10c1481494078736c1d5ba2c338872a6
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-03-20 15:17:58 +0530

    add exception check

--- leaderboard-broadcast/src/main/java/com/redhat/developers/service/LeaderboardBroadcasterService.java
+++ leaderboard-broadcast/src/main/java/com/redhat/developers/service/LeaderboardBroadcasterService.java
@@ -3,7 +3,9 @@ package com.redhat.developers.service;
 import static java.util.logging.Level.FINE;
 import static java.util.logging.Level.SEVERE;
 import java.time.Duration;
+import java.util.Collections;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.logging.Logger;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -53,12 +55,25 @@ public class LeaderboardBroadcasterService {
    */
   public CompletionStage<String> rankedPlayerList(long tick) {
     logger.log(FINE, "Sending message for tick {0} ", tick);
+    String playersList = jsonb.toJson(Collections.<String>emptyList());
+    AtomicReference<String> playersJson = new AtomicReference<>();
+    playersJson.set(playersList);
     return playerQueries
         .rankPlayers(client, rowCount)
-        .thenApply(p -> jsonb.toJson(p))
+        .thenApply(p -> {
+          try {
+            String strJson = jsonb.toJson(p);
+            playersJson.set(strJson);
+            logger.log(FINE, "Broadcast Leaderboard data {0} ",
+                strJson);
+          } catch (Exception e) {
+            logger.log(SEVERE, "Error building players json", e);
+          }
+          return playersJson.get();
+        })
         .exceptionally(e -> {
           logger.log(SEVERE, "Error retreiving player ranks ", e);
-          return "[]";
+          return playersJson.get();
         });
   }
 }
