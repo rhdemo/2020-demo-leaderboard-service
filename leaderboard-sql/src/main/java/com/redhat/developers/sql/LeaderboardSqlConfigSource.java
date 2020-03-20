d3f20dda262461ab498c80d34d2b193071fe6939
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-03-20 14:18:53 +0530

    (refactor) SPI use Config source

---
+++ leaderboard-sql/src/main/java/com/redhat/developers/sql/LeaderboardSqlConfigSource.java
@@ -0,0 +1,57 @@
+package com.redhat.developers.sql;
+
+import static java.util.logging.Level.FINE;
+import static java.util.logging.Level.SEVERE;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.logging.Logger;
+import org.eclipse.microprofile.config.spi.ConfigSource;
+
+/**
+ * SharedDataSourceConfigSource
+ */
+public class LeaderboardSqlConfigSource implements ConfigSource {
+
+  private Logger logger =
+      Logger.getLogger(LeaderboardSqlConfigSource.class.getName());
+
+  final Map<String, String> DS_MAP = new HashMap<>();
+
+  private void init() {
+    logger.log(FINE, "Loading properties form 'leaderboard-sql.properties'");
+    try {
+      Properties properties = new Properties();
+      properties
+          .load(this.getClass()
+              .getResourceAsStream("/leaderboard-sql.properties"));
+      for (String key : properties.stringPropertyNames()) {
+        String value = properties.getProperty(key);
+        DS_MAP.put(key, value);
+      }
+    } catch (IOException e) {
+      logger.log(SEVERE,
+          "Error Loading properties form 'leaderboard-sql.properties'");
+    }
+
+  }
+
+  @Override
+  public Map<String, String> getProperties() {
+    init();
+    return DS_MAP;
+  }
+
+  @Override
+  public String getValue(String propertyName) {
+    return DS_MAP.get(propertyName);
+  }
+
+  @Override
+  public String getName() {
+    return "leaderboard-sql-config-source";
+  }
+
+
+}
