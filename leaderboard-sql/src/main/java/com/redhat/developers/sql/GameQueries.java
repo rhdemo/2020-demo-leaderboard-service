37495fba792a9b3bac5ff33945b1162107491d1a
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-03-30 19:18:46 +0530

    fix date

--- leaderboard-sql/src/main/java/com/redhat/developers/sql/GameQueries.java
+++ leaderboard-sql/src/main/java/com/redhat/developers/sql/GameQueries.java
@@ -273,7 +273,7 @@ public class GameQueries {
     return OffsetDateTime.of(
         LocalDateTime.of(
             calendar.get(Calendar.YEAR),
-            calendar.get(Calendar.MONTH + 1),
+            calendar.get(Calendar.MONTH) + 1,
             calendar.get(Calendar.DAY_OF_MONTH),
             calendar.get(Calendar.HOUR_OF_DAY),
             calendar.get(Calendar.MINUTE),
