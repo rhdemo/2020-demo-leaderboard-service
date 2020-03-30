150f23f7d2c600dc7bdf2d75d109713a8e1d584f
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-03-30 07:06:31 +0530

    bump to mutiny

--- leaderboard-broadcast/src/main/java/com/redhat/developers/service/LeaderboardBroadcasterService.java
+++ leaderboard-broadcast/src/main/java/com/redhat/developers/service/LeaderboardBroadcasterService.java
@@ -20,22 +20,16 @@
 package com.redhat.developers.service;
 
 import static java.util.logging.Level.FINE;
-import static java.util.logging.Level.SEVERE;
 import java.time.Duration;
-import java.util.Collections;
-import java.util.concurrent.CompletionStage;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.logging.Logger;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.json.bind.Jsonb;
 import com.redhat.developers.sql.PlayerQueries;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 import io.smallrye.mutiny.Multi;
-import io.vertx.axle.pgclient.PgPool;
-import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonArray;
+import io.vertx.mutiny.pgclient.PgPool;
 
 /**
  * LeaderboardBroadcasterService
@@ -63,7 +57,7 @@ public class LeaderboardBroadcasterService {
     return Multi.createFrom()
         .ticks().every(Duration.ofSeconds(tickInterval))
         .on().overflow().drop()
-        .onItem().produceCompletionStage(this::rankedPlayerList).concatenate();
+        .onItem().produceMulti(this::rankedPlayerList).concatenate();
   }
 
   /**
@@ -71,24 +65,14 @@ public class LeaderboardBroadcasterService {
    * @param tick
    * @return
    */
-  public CompletionStage<JsonArray> rankedPlayerList(long tick) {
+  public Multi<JsonArray> rankedPlayerList(long tick) {
     logger.log(FINE, "Sending message for tick {0} ", tick);
-    AtomicReference<JsonArray> players = new AtomicReference<>();
     return playerQueries
         .rankPlayers(client, rowCount)
-        .thenApply(p -> {
-          try {
-            players.set(new JsonArray(p));
-            logger.log(FINE, "Broadcast Leaderboard data {0} ",
-                Json.encode(players));
-          } catch (Exception e) {
-            logger.log(SEVERE, "Error building players json", e);
-          }
-          return players.get();
+        .onItem()
+        .produceMulti(p -> {
+          return Multi.createFrom().item(new JsonArray(p));
         })
-        .exceptionally(e -> {
-          logger.log(SEVERE, "Error retreiving player ranks ", e);
-          return players.get();
-        });
+        .onFailure().recoverWithItem(new JsonArray());
   }
 }
